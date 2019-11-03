8059f6515aa0594c17a4c176cec534cacda383e4
TheGreatFatCat <thefatcatproductons@gmail.com>
2019-11-03 10:16:22 -0700

    Have regression working, I think, added some regression sets

--- RBFNet.py
+++ RBFNet.py
@@ -2,7 +2,135 @@ import numpy as np;
 from PAM import PAM
 from Cluster import KNN
 
+class RBFReg:
+    """
+          Class to function as an RBF regression network (one output node)
+          :param clusters, # of clusters (hidden nodes)
+          :param isReg,  bool to check if regression or not
+          :param learning_rate,  tuning paramater for our RBF net
+          :param maxruns, maximum amount of cycles we want the RBF to run for
 
+
+           """
+
+    def __init__(self,  clusters,  maxruns=1000, learning_rate=.01, ):
+
+        self.clusters = clusters
+        self.learning_rate = learning_rate
+        # weight array,  use out_nodes size 1  for reg
+        self.weights = np.random.uniform(-self.learning_rate, self.learning_rate, size=(self.clusters))
+        # bias term, vector of size out_nodes (so size 1 for reg as 1 output node)
+        self.bias = np.random.randn(1)
+        self.maxruns = maxruns
+        self.std = None
+        self.medoids = None
+
+    def gaus(self, x, c, s):
+            """ gausian"""
+            return np.exp(-1 / (2 * s ** 2) * (x - c) ** 2)
+
+    def calcHiddenOutputs(self, input, center, std):
+            knn = KNN
+            dist_between = knn.get_euclidean_distance(input, center)
+
+
+            return np.exp(-1 / (2 * std ** 2) * dist_between ** 2)
+
+
+    ":param medoids_list,  a list of medoids in a cluster"
+    # function to get max dist
+    def getMaxDist(self, medoids_list):
+        maxDist = 0
+        knn = KNN
+        for medoid in medoids_list:
+            for medoid2 in medoids_list:
+                # compare against all other medoids
+                curDist = knn.get_euclidean_distance(medoid.row, medoid2.row)
+                if curDist > maxDist:
+                    maxDist = curDist
+        return maxDist
+
+
+
+    """
+    :param data_set : data set to train on
+    :param expected_value : expected values for that data set
+    :param data_instance : instance of data class
+    """
+    def trainReg(self, data_set, expected_values, data_instance):
+        pam = PAM(k_val=self.clusters, data_instance=data_instance)
+        medoids_list = pam.assign_random_medoids(data_set, self.clusters)
+        pam.assign_data_to_medoids(data_set, medoids_list)
+        # set the STD of the clusters  (doing once for now)
+        self.std = self.getMaxDist(medoids_list) / np.sqrt(2 * self.clusters)
+        self.medoids = medoids_list
+        # train the regression model
+        converged = False
+        iterations = 0
+        while not converged:
+
+            if(iterations % 100 is 0):
+                print("Iteration: ")
+                print(iterations)
+            for index, row in data_set.iterrows():
+                # calculate the activation functions for each of the examples for each hidden node (cluster)
+                a = []
+
+                for medoid in medoids_list:
+                    medoidAct = self.calcHiddenOutputs(row, medoid.row, self.std)
+
+                    a.append(medoidAct)
+
+                a = np.array(a)
+                F = a.T.dot(self.weights) + self.bias
+                #backwards update pass
+
+                error = -(expected_values[index] - F)
+                self.weights = self.weights - self.learning_rate * a * error
+                self.bias = self.bias - self.learning_rate * error
+
+
+            if iterations > self.maxruns:
+                # break out if we hit the maximum runs
+                converged = True
+
+            iterations += 1
+
+
+    # function to predict values given current weights and a dataset
+    def predictReg(self, data_set):
+        predictions = []
+        for index, row in data_set.iterrows():
+            a = []
+
+            for medoid in self.medoids:
+                medoidAct = self.calcHiddenOutputs(row, medoid.row, self.std)
+
+                a.append(medoidAct)
+
+            a = np.array(a)
+            F = a.T.dot(self.weights) + self.bias
+
+            predictions.append(F.item())
+        return predictions
+
+
+    def mean_squared_error(self, predicted_data, actual_data):
+        """
+        :param predicted_data:  list of predicted values for datapoints (assume same order)
+        :param actual_data: actual values for those said data points  (assume same order)
+        :return MSE from the predicted data
+         """
+        n = len(actual_data)  # get out n for MSE
+        sum = 0  # sum of the MSE squared values
+
+        for (predict, true) in zip(predicted_data, actual_data): # go through all the points at the same time
+            currentSum = (true - predict) ** 2  # square it
+            sum += currentSum # add current to total sum
+
+        # divide by n
+        sum = sum/n
+        return sum # done, return sum
 
 class RBF:
         """
@@ -97,7 +225,7 @@ class RBF:
 
 
 
-                       
+
 
                         # TODO:  Impelemt the gradient descent rules using the activation values a as input
                         # backward pass
